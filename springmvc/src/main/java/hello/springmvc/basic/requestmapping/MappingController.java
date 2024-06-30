ec78b8ef2822bc73c3ae148f90db94c978e1ee3b
Jung JaeWoong <jjw_0609@naver.com>
2024-06-30 22:31:23 +0900

    HTTP 요청 메시지 - 단순 텍스트

--- springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -2,8 +2,8 @@ package hello.springmvc.basic.requestmapping;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MappingController {
